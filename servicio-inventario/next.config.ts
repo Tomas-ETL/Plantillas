3bb674e9319284544e7664750dfc3e3d6f2ca499
Johny-ctrl <81177794+Johny-ctrl@users.noreply.github.com>
2026-04-23 14:44:14 -0400

    Intento de vision en github pages

--- servicio-inventario/next.config.ts
+++ servicio-inventario/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Obligatorio para GitHub Pages
+  images: {
+    unoptimized: true, // GitHub Pages no soporta optimización de imágenes nativa de Next
+  },
 };
 
 export default nextConfig;
